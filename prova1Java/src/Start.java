444ea0ae395faa403e497daa76bbda96391f695a
cesare.papa <cesare.papa@I1W1101.majorana.locale>
2023-11-10 12:51:44 +0100

    1. modifica family friendly nel file

--- prova1Java/src/Start.java
+++ prova1Java/src/Start.java
@@ -4,8 +4,9 @@ public class Start {
 	public static void main(String[] args) {
 		System.out.println("ciao gente");
 		System.out.println("odio napoli");
-		System.out.println("Papa testa di c%$&0");
+		System.out.println("Papa testa di fiore");
 		System.out.println("altra modifica");
+		System.out.println("Dimeo non fare la bolitica");
 
 	}
 
