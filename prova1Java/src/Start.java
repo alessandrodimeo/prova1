a28fa46191e7bec62f3437194c28814af6552ab0
cesare.papa <cesare.papa@I1W1101.majorana.locale>
2023-11-10 12:40:10 +0100

    1. aggiunta di informazioni importanti al file

--- prova1Java/src/Start.java
+++ prova1Java/src/Start.java
@@ -4,6 +4,8 @@ public class Start {
 	public static void main(String[] args) {
 		System.out.println("ciao gente");
 		System.out.println("odio napoli");
+		
+		System.out.println("Dimeo scemo");
 	}
 
 }
