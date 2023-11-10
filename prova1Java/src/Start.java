0c3f1190fe0d5d0314117b5d00dd3c41c9befd2d
alessandro.di.meo <alessandro.di.meo@host.docker.internal>
2023-11-10 12:45:57 +0100

    Aggiunta di una informazione essenziale

--- prova1Java/src/Start.java
+++ prova1Java/src/Start.java
@@ -4,6 +4,7 @@ public class Start {
 	public static void main(String[] args) {
 		System.out.println("ciao gente");
 		System.out.println("odio napoli");
+		System.out.println("Papa testa di c%$&0");
 	}
 
 }
